7807bb117dd70ec686c376bb20df986ad58781ac
DamoM73 <damomurtagh@gmail.com>
2021-05-27 22:02:03 +1000

    Stage 1

---
+++ stage_1/main.py
@@ -0,0 +1,22 @@
+from room import Room
+
+# create rooms
+kitchen = Room("Kitchen")
+kitchen.set_description("A dank and dirty place, buzzing with flies")
+
+dining_hall = Room("Dining hall")
+dining_hall.set_description("A large room with ornate golden decorations on each wall")
+
+ballroom = Room("Ballroom")
+ballroom.set_description("A vast room with a shiny wooden floor; huge candlesitcks guard the entrance")
+
+# link rooms
+kitchen.set_linked_rooms(dining_hall,"south")
+dining_hall.set_linked_rooms(kitchen,"north")
+dining_hall.set_linked_rooms(ballroom,"west")
+ballroom.set_linked_rooms(dining_hall,"east")
+
+# describe the rooms
+kitchen.describe()
+dining_hall.describe()
+ballroom.describe()
